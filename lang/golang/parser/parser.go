5533b107bc16d5f15864e659c850b9959bdd5ef8
yinxuran.lucky <yinxuran.lucky@bytedance.com>
2025-12-27 09:37:13 +0800

    fix: replace local dir

--- lang/golang/parser/parser.go
+++ lang/golang/parser/parser.go
@@ -223,8 +223,15 @@ func getDeps(dir string, goWork bool) (a map[string]string, hasGoWork bool, cgoP
 		if len(mod.CgoFiles) > 0 {
 			cgoPkgs[module.Path] = true
 		}
+
 		if module.Replace != nil {
-			deps[module.Path] = module.Replace.Path + "@" + module.Replace.Version
+			if strings.HasPrefix(module.Replace.Path, "./") ||
+				strings.HasPrefix(module.Replace.Path, "../") ||
+				strings.HasPrefix(module.Replace.Path, "/") {
+				// local replace
+				deps[module.Path] = module.Path
+
+			}
 		} else {
 			if module.Version != "" {
 				deps[module.Path] = module.Path + "@" + module.Version
