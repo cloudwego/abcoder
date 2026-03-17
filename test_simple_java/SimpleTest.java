ec3d282cdbce20f0bb1a6bef567a5e62b4610449
lilong.21 <lilong.21@bytedance.com>
2026-03-17 18:42:24 +0800

    rm

--- test_simple_java/SimpleTest.java
+++
@@ -1,9 +0,0 @@
-public class SimpleTest {
-    public static void main(String[] args) {
-        System.out.println("Hello ABCoder!");
-    }
-    
-    public void testMethod() {
-        // This is a test method
-    }
-}
